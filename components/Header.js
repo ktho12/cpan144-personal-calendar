9d80cea5d2e4dd227d3f9170181fb32267200322
Katelyn <katelyn.thomas94@gmail.com>
2025-11-26 13:52:49 -0500

    Created basic header, basic styling

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import styles from "../styles/Header.module.css";
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <h1>Personal Calendar</h1>
+        </header>
+    );
+}
