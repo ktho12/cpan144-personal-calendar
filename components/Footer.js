7c0299eb9ab8b0f5978629ce372fb0c14b9a4b13
Katelyn <katelyn.thomas94@gmail.com>
2025-11-26 14:38:03 -0500

    Created basic footer and styling

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import styles from "../styles/Footer.module.css";
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p>&copy; 2025 Personal Calendar. A project by Katelyn Thomas</p>
+        </footer>
+    );
+}
