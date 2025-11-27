0c16413eef95d33e29239afd2d27980bb7775373
Katelyn <katelyn.thomas94@gmail.com>
2025-11-26 21:13:43 -0500

    commit outstanding changes

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import MainContent from "../components/MainContent";
 
+
 export default function Home() {
   return (
     <main>
-      
-      
+    
       <MainContent />
     </main>
   )
