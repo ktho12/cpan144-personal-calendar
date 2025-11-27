4aaff8572d005d4cc176250a5fc0992f9dd73c07
Katelyn <katelyn.thomas94@gmail.com>
2025-11-26 22:08:50 -0500

    Added styling to home page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import MainContent from "../components/MainContent";
+import styles from "../styles/Home.module.css";
 
 
 export default function Home() {
   return (
-    <main>
-    
+    <main className={styles.homeMain}>
+      <div className={styles.formWrapper}>
       <MainContent />
+      </div>
     </main>
   )
 }
