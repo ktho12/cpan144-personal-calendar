89b412e7e910c51daf3ca0c90000c0bda91f57d5
Katelyn <katelyn.thomas94@gmail.com>
2025-12-12 17:00:19 -0500

    Added logo to header

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,16 @@
 import styles from "../styles/Header.module.css";
+import Link from "next/link";
 
 export default function Header() {
     return (
         <header className={styles.header}>
-            <h1>Personal Calendar</h1>
+        <Link href="/">
+        <img
+            src="/logov1.svg"
+            alt="Personal Calendar Logo"
+            className={styles.logo}
+        />
+      </Link>
         </header>
     );
 }
