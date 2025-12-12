0624ec6f146cafe048793cca251016fc4332aa84
Katelyn <katelyn.thomas94@gmail.com>
2025-12-12 16:09:34 -0500

    Added dynamic/lazy loading for weather data

--- pages/Weather.js
+++ pages/Weather.js
@@ -1,5 +1,14 @@
-import WeatherApi from '../components/WeatherAPI';
 import styles from '../styles/weatherPage.module.css';
+import dynamic from "next/dynamic";
+
+const WeatherApi = dynamic(
+    () => import("../components/WeatherAPI"),
+    {
+        loading: () => <p>Loading weather...</p>,
+        ssr: false,
+  }
+);
+
 
 export default function WeatherData() {
     return (
